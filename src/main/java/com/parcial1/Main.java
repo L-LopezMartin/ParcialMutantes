b7b688a7d76d48eea772de76eb596fbb3b5e96e8
Leandro Lopez <leanlopezmartin@gmail.com>
2024-10-28 00:31:45 -0300

    Limpieza del main y comentadas operaciones no usadas de Persona

--- src/main/java/com/parcial1/Main.java
+++ src/main/java/com/parcial1/Main.java
@@ -18,64 +18,10 @@ import java.util.List;
 
 @SpringBootApplication
 public class Main {
-    private static final Logger logger = LoggerFactory.getLogger(Main.class);
-
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
-        @Bean
-        @Transactional
-        CommandLineRunner init(PersonaRepository personaRepository){
-            return args ->{
-
-                Persona persona = new Persona();
-                persona.genomaAleatorio(12);
-                persona.printGenoma();
-                //persona.isMutante();
-                //System.out.println(persona.isMutant());
-
-
-                Persona persona1 = new Persona();
-                String[] matriz4 = {
-                        "ATTC",
-                        "CACC",
-                        "GGAG",
-                        "TTTA",
-                };
-                persona1.setGenoma(matriz4);
-                persona1.printGenoma();
-                //persona1.isMutante();
-                //System.out.println(persona1.isMutant());
-
-                Persona persona2 = new Persona();
-                String[] matriz1 = {
-                        "TGAGTGGACT",
-                        "TATAGGCTTA",
-                        "GTCCCCATGC",
-                        "TCTGTTAAGA",
-                        "ATATAGGATT",
-                        "AAACGTAATC",
-                        "CAGCTGAGGG",
-                        "CACGCAGGAG",
-                        "CAGCCTGTTG",
-                        "TGTCAATGCA"
-                };
-                persona2.setGenoma(matriz1);
-                persona2.printGenoma();
-
-                Persona persona3 = new Persona();
-                String[] matriz2 = {
-                        "TGAGA",
-                        "GTCCC",
-                        "GATCC",
-                        "GACTT",
-                        "AGCCT"
-                };
-                persona3.setGenoma(matriz2);
-                persona3.printGenoma();
-            };
-        }
 }
 
 
--- src/main/java/com/parcial1/entities/Persona.java
+++ src/main/java/com/parcial1/entities/Persona.java
@@ -24,36 +24,32 @@ public class Persona extends Base{
     private boolean isMutant = false;
 
     //Genera una matriz cuadrada con la dimensión que se le envíe
-    public void genomaAleatorio(int dim){
-        Random random = new Random();
-        genoma = new String[dim];
-        for (int i=0;i< dim;i++){
-            genoma[i] = "";
-            for (int j = 0; j < dim; j++) {
-                int k = random.nextInt(posiblesValores.length());
-                genoma[i] = genoma[i] + posiblesValores.charAt(k);
-            }
-        }
-    }
+//    public void genomaAleatorio(int dim){
+//        Random random = new Random();
+//        genoma = new String[dim];
+//        for (int i=0;i< dim;i++){
+//            genoma[i] = "";
+//            for (int j = 0; j < dim; j++) {
+//                int k = random.nextInt(posiblesValores.length());
+//                genoma[i] = genoma[i] + posiblesValores.charAt(k);
+//            }
+//        }
+//    }
 
     //Imprime la matriz en consola
-    public void printGenoma(){
-        int length = genoma.length;
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < length; j++) {
-                System.out.print(genoma[i].charAt(j) + ",");
-            }
-            System.out.println();
-        }
-    }
+//    public void printGenoma(){
+//        int length = genoma.length;
+//        for (int i = 0; i < length; i++) {
+//            for (int j = 0; j < length; j++) {
+//                System.out.print(genoma[i].charAt(j) + ",");
+//            }
+//            System.out.println();
+//        }
+//    }
 
     //Colocar un nuevo genoma a la persona marca la persona como no verificada
     public void setGenoma(String[] input){
         this.genoma = input;
         isMutant = false;
-    }
-
-    public Persona(String[] genoma){
-        this.genoma = genoma;
     }
 }
